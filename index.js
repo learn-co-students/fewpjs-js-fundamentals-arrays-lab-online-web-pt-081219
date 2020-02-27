b055ad1453a521abf967bedc022be62bd609e320
github username <github email address>
2020-02-26 21:54:02 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution here!
+const 
+append = ["Milo", "Otis", "Garfield"],
+prepend = ["Milo", "Otis", "Garfield"],
+removeLast = ["Milo", "Otis", "Garfield"],
+removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.push('Odie')
+prepend.splice(0,0, "Odie");
+removeLast.splice(-1,1);
+removeFirst.splice(0,1);
